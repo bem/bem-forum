710808bc495bed3a9700aeef786bf51446ee6c9c
Liza Golubenko <xli3kox@gmail.com>
2017-07-04 09:38:12 +0300

    interface changes

--- common.blocks/issues/issues.bemtree.js
+++ common.blocks/issues/issues.bemtree.js
@@ -5,12 +5,17 @@ block('issues').content()(function() {
     return [
         {
             elem: 'content',
-            content: data.issues.map(issue => ({
+            content: data.issues.length ? data.issues.map(issue => ({
                 block: 'issue',
                 mix: { block, elem: 'issue' },
                 mods: { state: issue.state, counts: this.data.issues.length === 1 },
-                issue: issue
-            }))
+                issue: issue,
+                userLogin: data.userLogin
+            })) : {
+                block: 'message',
+                mix: { block, elem: 'message' },
+                content: 'No posts found'
+            }
         },
         (pagination.next || pagination.prev) && {
             block: 'pager',
